d1327a1df49cfd27c13d8f99214e6f01f6e15353
sosok11let <118977309+sosok11let@users.noreply.github.com>
2023-01-19 19:24:04 +0300

    Update Game_run.py
    The main program

--- Project/Game_run.py
+++ Project/Game_run.py
@@ -139,3 +139,121 @@ def you_lost():
         print_text("GAME OVER", 185, 80, (255, 0, 0))
         pygame.display.update()
     pygame.quit()
+    
+    
+def star_game():
+    global score
+
+    pygame.init()
+    size = width, height = 1200, 800
+    screen = pygame.display.set_mode(size)
+
+    image = load_image('fon.png')
+    image2 = load_image('platforma_11.png')
+    image3 = load_image('platforma_2.png')
+    image4 = load_image('platforma_3.png')
+    ship_top = screen.get_height() - image.get_height()
+    ship_left = screen.get_width() / 2 - image.get_width() / 2
+
+    image_jump = pygame.image.load('Animation_super_player/player_left_jump.png')
+    image_stay = pygame.image.load('Animation_super_player/player_left_4.png')
+
+    image_jump_right = pygame.image.load('Animation_super_player/player_right_jump.png')
+    image_stay_right = pygame.image.load('Animation_super_player/player_right_4.png')
+
+    flagbullets = True
+    platf = []
+    pl1 = Platforms()
+    pl2 = Platforms_2()
+    pl3 = Platforms_3()
+    platf.append(pl1)
+    platf.append(pl2)
+    platf.append(pl3)
+    up = False
+    player = Player(55, 55)
+
+    left = False
+    right = False
+
+    flagshot = False
+
+    ak47 = pygame.image.load("data/glock.png")
+    ak47_2 = ak47.get_rect(center=screen.get_rect().center)
+
+    def blit_point_to_mouse(target_surf, char_surf, x, y):
+        mx, my = pygame.mouse.get_pos()
+        dir = (mx - x, my - y)
+        length = math.hypot(*dir)
+        if length == 0.0:
+            dir = (0, -1)
+        else:
+            dir = (dir[0] / length, dir[1] / length)
+        angle = math.degrees(math.atan2(-dir[1], dir[0]))
+        rotated_surface = pygame.transform.rotate(char_surf, angle)
+        rotated_surface_location = rotated_surface.get_rect(center=(player.rect.x + 25, player.rect.y + 35))
+        target_surf.blit(rotated_surface, rotated_surface_location)
+
+    pygame.mouse.set_visible(False)
+
+    running = True
+
+    randomspeedufo = [3, 4, 5, 6, 7, 8]
+    for i in range(6):
+        randomspeedufo_choise = random.choice(randomspeedufo)
+        mob = Mobs(screen, randomspeedufo_choise)
+        randomspeedufo.remove(randomspeedufo_choise)
+
+    bullets = []
+    pos = (250, 250)
+    pygame.mixer.music.play(-1)
+
+    leftorright = False
+
+    font = pygame.font.Font('data/font_super_game.ttf', 30)
+
+    hp = 3
+    hp_max = 5
+    hp_x = 990
+    hp_player = pygame.image.load('data/heart.png')
+    score = 0
+
+    bullets_count = 10
+
+    def print_hp(hp, hp_x):
+        if hp < hp_max:
+            for i in range(hp):
+                screen.blit(hp_player, (hp_x, 630))
+                hp_x += 70
+
+    while running:
+        pygame.time.delay(17)
+        ax, ay, bb, ss = player.rect
+        pos = ax + 20, ay + 30
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                if score > int(f):
+                    open('txt_files/record.txt', 'w').write(str(score))
+                running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_a:
+                    left = True
+                if event.key == pygame.K_d:
+                    right = True
+                if event.key == pygame.K_SPACE:
+                    up = True
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_a:
+                    left = False
+                if event.key == pygame.K_d:
+                    right = False
+                if event.key == pygame.K_SPACE:
+                    up = False
+            if event.type == pygame.MOUSEBUTTONUP:
+                if event.button == 1 and flagshot:
+                    bull = Bullet(*pos)
+                    bullets.append(bull)
+                    player_sprites.add(bull)
+                    shot.play()
+                    bullets_count -= 1
+                elif not flagshot:
+                    flagshot = True

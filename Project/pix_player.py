3b8d49d19cb49115dbce1ce7b3e26bcf0d9d8d02
sosok11let <118977309+sosok11let@users.noreply.github.com>
2023-01-19 19:18:38 +0300

    Update pix_player.py
    Bullets

--- Project/pix_player.py
+++ Project/pix_player.py
@@ -239,3 +239,30 @@ class Mobs(pygame.sprite.Sprite):
 
     def ret(self):
         return self.rect
+    
+    
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__(player_sprites)
+        self.pos = (x, y)
+        mx, my = pygame.mouse.get_pos()
+        self.dir = (mx - x, my - y)
+        length = math.hypot(*self.dir)
+        if length == 0.0:
+            self.dir = (0, -1)
+        else:
+            self.dir = (self.dir[0]/length, self.dir[1]/length)
+        angle = math.degrees(math.atan2(-self.dir[1], self.dir[0]))
+        self.bullet = pygame.Surface((20, 4)).convert_alpha()
+        self.bullet.fill((255, 155, 0))
+        self.bullet = pygame.transform.rotate(self.bullet, angle)
+        self.rect = self.bullet.get_rect(center=self.pos)
+        self.speed = 30
+
+    def update(self):
+        self.pos = (self.pos[0]+self.dir[0]*self.speed,
+                    self.pos[1]+self.dir[1]*self.speed)
+
+    def draw(self, screen):
+        self.rect = self.bullet.get_rect(center=self.pos)
+        screen.blit(self.bullet, self.rect)
